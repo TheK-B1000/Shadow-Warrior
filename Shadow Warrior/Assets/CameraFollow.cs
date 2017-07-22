2ec076618cd343fd4772eeed352531f6e1b0cb29
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2017-07-21 22:31:06 -0400

    03 - Raycasting and Click Mouse to Move

--- Shadow Warrior/Assets/CameraFollow.cs
+++ Shadow Warrior/Assets/CameraFollow.cs
@@ -10,7 +10,7 @@ public class CameraFollow : MonoBehaviour {
 
 
 	void Start (){
-		offset = transform.position - player.transform.position;
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 
 	void LateUpdate(){
