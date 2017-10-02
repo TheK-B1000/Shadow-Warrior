683c14d6d8ded8baa694f19dcdbb78709953932d
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2017-10-02 14:14:24 -0400

    13 - Enemy move and attack spheres

--- Shadow Warrior/Assets/Characters/Enemies/Enemy.cs
+++ Shadow Warrior/Assets/Characters/Enemies/Enemy.cs
@@ -8,11 +8,13 @@ public class Enemy : MonoBehaviour, IDamageable {
 
 
 	[SerializeField] float maxHealthPoints = 100f;
-	[SerializeField] float attackRadius = 5.0f;
+	[SerializeField] float attackRadius = 10.0f;
+	[SerializeField] float chaseRadius = 5.0f;
 
 	float currentHealthPoints = 100f;
 	AICharacterControl aiCharacterControl = null;
 	GameObject player = null;
+	Projectile projectile;
 
 	public float healthAsPercentage
 	{
@@ -26,16 +28,37 @@ public class Enemy : MonoBehaviour, IDamageable {
 		player = GameObject.FindGameObjectWithTag ("Player");
 		aiCharacterControl = GetComponent<AICharacterControl> ();
 	}
-		
 
-
+	// TODO CLEAN UP
+	//void LaunchProjectile(){
+	//	GameObject.projectile = 
+	//		if(Layerhit = attackRadius)
+	//		{
+	//			fire(projectile);
+	//		}
+	//}
 
 	void Update (){
 
 		float distanceToPlayer = Vector3.Distance (player.transform.position, transform.position);
-		if (distanceToPlayer <= attackRadius) {
-			aiCharacterControl.SetTarget (player.transform);
-		} else {
+		if (distanceToPlayer <= attackRadius) 
+		{
+			print (gameObject.name + "attacking player");
+			//TODO spawn projectile
+		} 
+		else
+		{
+			aiCharacterControl.SetTarget (transform);
+		}
+			
+
+		if (distanceToPlayer <= chaseRadius) 
+		{
+			print (gameObject.name + "move to player");
+			aiCharacterControl.SetTarget(player.transform);
+		} 
+		else 
+		{
 			aiCharacterControl.SetTarget (transform);
 		}
 	}
@@ -43,5 +66,16 @@ public class Enemy : MonoBehaviour, IDamageable {
 	public void TakeDamage(float damage)
 	{
 		currentHealthPoints = Mathf.Clamp (currentHealthPoints - damage, 0f, maxHealthPoints);
+	}
+
+	void OnDrawGizmos()
+	{
+		// Draw move sphere
+		Gizmos.color = new Color (0f , 0f, 240f, .5f);
+		Gizmos.DrawWireSphere (transform.position, chaseRadius);
+
+		// Draw attack sphere
+		Gizmos.color = new Color (255f, 0f, 0, .5f);
+		Gizmos.DrawWireSphere (transform.position, attackRadius);
 	}
 }
