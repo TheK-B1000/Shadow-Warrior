8adc6d40dec3f7aab137af8a399fb4cee94f6724
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2017-10-05 18:49:50 -0400

    25 - Problems with Prefabs

--- Shadow Warrior/Assets/Characters/Player/Player.cs
+++ Shadow Warrior/Assets/Characters/Player/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour, IDamageable {
 
@@ -14,8 +15,6 @@ public class Player : MonoBehaviour, IDamageable {
 	[SerializeField] float maxAttackRange = 2f;
 
 	[SerializeField] Weapon weaponInUse;
-	[SerializeField] GameObject weaponSocketRight;
-	[SerializeField] GameObject weaponSocketLeft;
 
 
 	GameObject currentTarget;
@@ -35,12 +34,22 @@ public class Player : MonoBehaviour, IDamageable {
 	private void PutWeaponInHand()
 	{
 		var weaponPrefab = weaponInUse.GetWeaponPrefab();
-		var weapon = Instantiate(weaponPrefab, weaponSocketRight.transform, weaponSocketLeft.transform);
+		GameObject dominantHand = RequestDominantHand ();
+		var weapon = Instantiate(weaponPrefab, dominantHand.transform);
 
 		// TODO move to correct place and child to hand
 		weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
 		weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
 
+	}
+
+	private GameObject RequestDominantHand()
+	{
+		var dominantHands = GetComponentsInChildren<DominantHand>();
+		int numberOfDominantHands = dominantHands.Length;
+		Assert.AreNotEqual (numberOfDominantHands, 0, "No DominantHand found on Player ");
+		Assert.IsFalse (numberOfDominantHands  > 1, "Multiple DominantHand Scripts found on Player, Please remove one ")
+	
 	}
 
 	private void RegisterForMouseClick()
