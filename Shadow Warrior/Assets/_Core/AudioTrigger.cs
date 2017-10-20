492d18e85fc5848f19423c51e40762ab67a61640
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2017-10-20 16:18:25 -0400

    89 - Achieving Audio Awesomness

--- Shadow Warrior/Assets/_Core/AudioTrigger.cs
+++ Shadow Warrior/Assets/_Core/AudioTrigger.cs
@@ -2,13 +2,16 @@
 
 public class AudioTrigger : MonoBehaviour {
 
+	//Serialized
 	[SerializeField] AudioClip clip;
-	[SerializeField] int layerFilter = 0;
-	[SerializeField] float triggerRadius = 5f;
+	[SerializeField] int layerFilter = 11;
+	[SerializeField] float playerDistanceThreshold = 2f;
 	[SerializeField] bool isOneTimeOnly = true;
 
+	//Private members
 	[SerializeField] bool hasPlayed = false;
 	AudioSource audioSource;
+	GameObject player; // will only trigger on distance to player
 
 	void Start()
 	{
@@ -16,17 +19,14 @@ public class AudioTrigger : MonoBehaviour {
 		audioSource.playOnAwake = false;
 		audioSource.clip = clip;
 
-		SphereCollider sphereCollider = gameObject.AddComponent<SphereCollider>();
-		sphereCollider.isTrigger = true;
-		sphereCollider.radius = triggerRadius;
-		gameObject.layer = LayerMask.NameToLayer ("Ignore Raycast");
 	}
 
-	void OnTriggerEnter(Collider other)
+	void Update()
 	{
-		if (other.gameObject.layer == layerFilter)
+		float distanceToPlayer = Vector3.Distance (transform.position, player.transform.position);
+		if (distanceToPlayer <= playerDistanceThreshold)
 		{
-			RequestPlayAudioClip();
+			return;
 		}
 	}
 
@@ -46,7 +46,7 @@ public class AudioTrigger : MonoBehaviour {
 	void OnDrawGizmos()
 	{
 		Gizmos.color = new Color(0, 255f, 0, .5f);
-		Gizmos.DrawWireSphere(transform.position, triggerRadius);
+		Gizmos.DrawWireSphere(transform.position, playerDistanceThreshold);
 	}
 }
 
